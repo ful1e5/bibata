00f70bbbe40caca96e4ea41baa1a22902a39cb29
Abdulkaiz Khatri <24286590+ful1e5@users.noreply.github.com>
2023-12-29 18:17:14 +0530

    feat(app): git commit and branch information added in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 import {
@@ -18,7 +19,19 @@ type Props = {};
 
 // eslint-disable-next-line no-unused-vars
 export const Footer: React.FC<Props> = (_props) => {
-  const gitCommintSHA = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+  const path = usePathname();
+
+  const devEnv = process.env.VERCEL_ENV === 'development';
+  const previewEnv = process.env.VERCEL_ENV === 'preview';
+
+  const gitCommintSHA = devEnv
+    ? 'main'
+    : process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+
+  const swtichBranchUrl = previewEnv
+    ? 'https://www.bibata.live'
+    : 'https://dev.bibata.live';
+
   return (
     <footer className='bg-[--bg-dark] border-t border-white/[.1] p-3 mt-20'>
       <div className='container mx-auto'>
@@ -91,18 +104,36 @@ export const Footer: React.FC<Props> = (_props) => {
               className='inline-flex gap-1 hover:text-white hover:underline'>
               {`v${LIB_VERSION}`}
             </Link>
+          </div>
 
+          <div className='mt-3 flex items-center justify-start gap-2 font-mono tracking-tighter text-sm text-white/[.89]'>
             {gitCommintSHA && (
               <>
+                <div className='inline-flex gap-1'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='fill-current w-3'
+                    viewBox='0 0 24 24'>
+                    <path d='M21 3c0-1.657-1.343-3-3-3s-3 1.343-3 3c0 1.323.861 2.433 2.05 2.832.168 4.295-2.021 4.764-4.998 5.391-1.709.36-3.642.775-5.052 2.085v-7.492c1.163-.413 2-1.511 2-2.816 0-1.657-1.343-3-3-3s-3 1.343-3 3c0 1.305.837 2.403 2 2.816v12.367c-1.163.414-2 1.512-2 2.817 0 1.657 1.343 3 3 3s3-1.343 3-3c0-1.295-.824-2.388-1.973-2.808.27-3.922 2.57-4.408 5.437-5.012 3.038-.64 6.774-1.442 6.579-7.377 1.141-.425 1.957-1.514 1.957-2.803zm-16.8 0c0-.993.807-1.8 1.8-1.8s1.8.807 1.8 1.8-.807 1.8-1.8 1.8-1.8-.807-1.8-1.8zm3.6 18c0 .993-.807 1.8-1.8 1.8s-1.8-.807-1.8-1.8.807-1.8 1.8-1.8 1.8.807 1.8 1.8z' />
+                  </svg>
+                  <Link
+                    target='_blank'
+                    href={`https://github.com/ful1e5/bibata/commit/${gitCommintSHA}`}
+                    className='inline-flex gap-1 hover:text-white hover:underline'>
+                    {`${gitCommintSHA.substring(0, 6)}`}
+                  </Link>
+                </div>
+
                 <p>•</p>
-                <Link
-                  target='_blank'
-                  href={`https://github.com/ful1e5/bibata/commit/${gitCommintSHA}`}
-                  className='inline-flex gap-1 hover:text-white hover:underline'>
-                  {`${gitCommintSHA.substring(0, 6)}`}
-                </Link>
               </>
             )}
+
+            <Link
+              target='_blank'
+              href={`${swtichBranchUrl}${path}`}
+              className='inline-flex gap-1 hover:text-white hover:underline'>
+              {`Switch to ${previewEnv ? 'Upstream' : 'Development'}`}
+            </Link>
           </div>
 
           <div className='mt-7 pt-3 border-t border-white/[.1] flex flex-col'>
