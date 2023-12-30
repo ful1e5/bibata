6f4fcab82a3ef9d6918dbfa0b1aaea5651919a5e
Abdulkaiz Khatri <24286590+ful1e5@users.noreply.github.com>
2023-12-30 22:30:23 +0530

    feat(app): attach Commit SHA in bug reports

--- src/utils/bug-report.ts
+++ src/utils/bug-report.ts
@@ -1,6 +1,14 @@
 export const bugReportTemplate = (title: string, logs: any): string => {
+  const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+
   return encodeURIComponent(`### Error
 ${title}
+
+${
+  sha &&
+  `### Refrence
+https://github.com/ful1e5/bibata/commit/${sha}`
+}
 
 ### Logs
 
